df46dd0e4bbf1b969d13203ddfb4555a710c0fbf
ali.abukahil <ali.abukahil@gmail.com>
2021-06-16 19:17:05 +0200

    5. FaqSections

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from 'react'
 // importing Components
 import AboutSection from '../components/AboutSection';
 import ServicesSection from "../components/ServicesSection";
+import FaqSections from '../components/FaqSections';
 
 function AboutUs() {
     return (
     <>
         <AboutSection />
         <ServicesSection />
+        <FaqSections />
     </>
     )
 }
