950aca6fe7e2344d294adf5f2fe6aa011a2afa12
ali.abukahil <ali.abukahil@gmail.com>
2021-06-19 17:01:14 +0200

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 // Importing Global Styles from Styled Components
-import GlobalStyles from './components/GlobalStyles';
+import GlobalStyles from "./components/GlobalStyles";
 // Importing Pages
-import AboutUs from './pages/AboutUs';
+import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   // The rendering section
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <AboutUs />
     </div>
-  )
+  );
 }
 
 export default App;
-
