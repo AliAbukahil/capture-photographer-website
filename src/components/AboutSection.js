a66c0d706b65a11e0435b4667672b09e1ad9eece
ali.abukahil <ali.abukahil@gmail.com>
2021-06-26 00:07:18 +0200

    framer motion Animation is Cool

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,6 +4,7 @@ import home1 from "../img/home1.png";
 
 // Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 import {
   StyledAbout,
@@ -13,47 +14,34 @@ import {
 } from "../styles";
 
 function AboutSection() {
-  //   const titleAnim = {
-  //     hidden: { opacity: 0 },
-  //     show: { opacity: 1, transition: { duration: 2 } },
-  //   };
-
-  //   const container = {
-  //     hidden: { x: 100 },
-  //     show: {
-  //       x: 0,
-  //       transition: {
-  //         duration: 0.75,
-  //         ease: "easeOut",
-  //         staggerChildren: 1,
-  //       },
-  //     },
-  //   };
-
   return (
     <StyledAbout>
       <StyledDescription>
         <motion.div>
           <StyledHide>
-            <motion.h2>I work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>I work to make</motion.h2>
           </StyledHide>
           <StyledHide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>ideas</span> come to
             </motion.h2>
           </StyledHide>
           <StyledHide>
-            <motion.h2>life.</motion.h2>
+            <motion.h2 variants={titleAnim}>life.</motion.h2>
           </StyledHide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Full Stack Web Developer. Contact me for any photography or
           videography ideas that you have.
-        </p>
-        <button>Contact Me</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Me</motion.button>
       </StyledDescription>
       <StyledImage className="image">
-        <img src={home1} alt="man with a a camera" />
+        <motion.img
+          variants={photoAnim}
+          src={home1}
+          alt="man with a a camera"
+        />
       </StyledImage>
     </StyledAbout>
   );
--- src/styles.js
+++ src/styles.js
@@ -1,36 +1,34 @@
 import styled from "styled-components";
 
-// Syntax of the Styled Components 
+// Syntax of the Styled Components
 // Please note that you can use sass syntax here too!
-export const StyledAbout = styled.div `
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
+export const StyledAbout = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
 `;
 
-export const StyledDescription = styled.div `
-    flex: 1;
-    padding-right:5rem;
-    h2{
-        font-weight: lighter;
-    }
+export const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
 `;
 
-export const StyledImage = styled.div `
-    flex: 1;
-    overflow: hidden; // for the animation of the div img
-    img{
-        width: 85%;
-        height: 70vh;
-        object-fit: cover;
-        border-radius: 3rem 0.9rem 3rem 0.9rem;
-    } 
+export const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden; // for the animation of the div img
+  img {
+    width: 85%;
+    height: 70vh;
+    object-fit: cover;
+  }
 `;
 
-export const StyledHide = styled.div `
-    overflow: hidden;
+export const StyledHide = styled.div`
+  overflow: hidden;
 `;
-
